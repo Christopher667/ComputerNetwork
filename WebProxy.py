328739083a6fa5743931fdae50340e83a214ef7e
Christopher667 <hellobjtu@gmail.com>
2023-11-28 15:06:15 +0800

    代码初步构建，并对其debug以解决localhost无法发送信息问题。

---
+++ WebProxy.py
@@ -0,0 +1,95 @@
+import os
+import socket
+
+
+def handleReq(clientSocket):
+    # recv data
+    # find the fileName
+    # judge if the file named "fileName" if existed
+    # if not exists, send req to get it
+    try:
+        recvData = clientSocket.recv(1024).decode("UTF-8")
+        print("Debug: Received data from client:", recvData)
+
+        # Split the received data into lines
+        requestLines = recvData.split("\r\n")
+
+        # Check if the request has at least one line
+        if not requestLines:
+            print("Error: Empty request received from client.")
+            return
+
+        # Extract the first line of the request
+        reqHeaderLine = requestLines[0]
+        print("Debug: Request header line:", reqHeaderLine)
+
+        # Extract the requested file name from the first line
+        fileName = reqHeaderLine.split()[1].split("//")[1].replace('/', '')
+        print("Debug: Processed fileName:", fileName)
+        print("fileName: " + fileName)
+        filePath = "./" + fileName.split(":")[0].replace('.', '_')
+    except Exception as e:
+        print("Error: {0}".format(e))
+
+    try:
+        file = open(filePath + "./index.html", 'rb')
+        print("File is found in proxy server.")
+        #responseMsg = file.readlines()
+        #for i in range(0, len(responseMsg)):
+           # clientSocket.sendall(responseMsg[i])
+        responseMsg = file.read()
+        clientSocket.sendall(responseMsg)
+        print("Send, done.")
+    except:
+        print("File is not exist.\nSend request to server...")
+        try:
+            proxyClientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            serverName = fileName.split(":")[0]
+            proxyClientSocket.connect((serverName, 80))
+            proxyClientSocket.sendall(recvData.encode("UTF-8"))
+            responseMsg = proxyClientSocket.recv(4069)
+            print("File is found in server.")
+            clientSocket.sendall(responseMsg)
+            print("Send, done.")
+            # cache
+            if not os.path.exists(filePath):
+                os.makedirs(filePath)
+            cache = open(filePath + "./index.html", 'w')
+            cache.writelines(responseMsg.decode("UTF-8").replace('\r\n', '\n'))
+            cache.close()
+            print("Cache, done.")
+        except:
+            print("Connect timeout.")
+
+
+def startProxy(port):
+    proxyServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    proxyServerSocket.bind(("", port))
+    proxyServerSocket.listen(0)
+    while True:
+        try:
+            print("Proxy is waiting for connecting...")
+            clientSocket, addr = proxyServerSocket.accept()
+            print("Connect established")
+            handleReq(clientSocket)
+            clientSocket.close()
+        except Exception as e:
+            print("Error: {0}".format(e))
+            break
+    proxyServerSocket.close()
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            port = int(input("choose a port number over 1024:"))
+        except ValueError:
+            print("Please input an integer rather than {0}".format(type(port)))
+            continue
+        else:
+            if port <= 1024:
+                print("Please input an integer greater than 1024")
+                continue
+            else:
+                break
+    startProxy(port)
